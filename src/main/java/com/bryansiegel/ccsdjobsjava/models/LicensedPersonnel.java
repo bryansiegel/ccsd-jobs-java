24d7995fbd2ae06cec3c3e20d01d624c63c0e028
Bryan Siegel <bryansiegel@gmail.com>
2025-03-25 11:18:52 -0700

    LicensedPersonnel Model

--- src/main/java/com/bryansiegel/ccsdjobsjava/models/LicensedPersonnel.java
+++ src/main/java/com/bryansiegel/ccsdjobsjava/models/LicensedPersonnel.java
@@ -4,31 +4,143 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class LicensedPersonnel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+    private String jobCode;
+    private String division;
+    private String classification;
+    private String termsOfEmployment;
+    private String flsaStatus;
+    private String positionSummary;
+    private String essentialDutiesAndResponsibilities;
+    private String positionExpectations;
+    private String positionRequirements;
 
-    //job code
-    //division
-    //classification
-    //terms of employment
-    //FLSA status
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
-    //position summary
-    //essential duties and responsibilities
-    //position expectations
+    public Long getId() {
+        return id;
+    }
 
-    //position requirements // don't create
-    //education and training
-    //licenses and certifications
-    //preferred qualifications
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    //Job Revision Information // don't create
-    //revised
-    //created
+    public String getJobCode() {
+        return jobCode;
+    }
+
+    public void setJobCode(String jobCode) {
+        this.jobCode = jobCode;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getTermsOfEmployment() {
+        return termsOfEmployment;
+    }
+
+    public void setTermsOfEmployment(String termsOfEmployment) {
+        this.termsOfEmployment = termsOfEmployment;
+    }
+
+    public String getFlsaStatus() {
+        return flsaStatus;
+    }
+
+    public void setFlsaStatus(String flsaStatus) {
+        this.flsaStatus = flsaStatus;
+    }
+
+    public String getPositionSummary() {
+        return positionSummary;
+    }
+
+    public void setPositionSummary(String positionSummary) {
+        this.positionSummary = positionSummary;
+    }
+
+    public String getEssentialDutiesAndResponsibilities() {
+        return essentialDutiesAndResponsibilities;
+    }
+
+    public void setEssentialDutiesAndResponsibilities(String essentialDutiesAndResponsibilities) {
+        this.essentialDutiesAndResponsibilities = essentialDutiesAndResponsibilities;
+    }
+
+    public String getPositionExpectations() {
+        return positionExpectations;
+    }
+
+    public void setPositionExpectations(String positionExpectations) {
+        this.positionExpectations = positionExpectations;
+    }
+
+    public String getPositionRequirements() {
+        return positionRequirements;
+    }
+
+    public void setPositionRequirements(String positionRequirements) {
+        this.positionRequirements = positionRequirements;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "LicensedPersonnel{" +
+                "id=" + id +
+                ", jobCode='" + jobCode + '\'' +
+                ", division='" + division + '\'' +
+                ", classification='" + classification + '\'' +
+                ", termsOfEmployment='" + termsOfEmployment + '\'' +
+                ", flsaStatus='" + flsaStatus + '\'' +
+                ", positionSummary='" + positionSummary + '\'' +
+                ", essentialDutiesAndResponsibilities='" + essentialDutiesAndResponsibilities + '\'' +
+                ", positionExpectations='" + positionExpectations + '\'' +
+                ", positionRequirements='" + positionRequirements + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
