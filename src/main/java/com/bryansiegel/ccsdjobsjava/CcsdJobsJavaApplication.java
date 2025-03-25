4bd4cd456a9024ea415c3ca19fb4bdcb8c9b84e6
Bryan Siegel <bryansiegel@gmail.com>
2025-03-25 10:03:13 -0700

    initial commit

---
+++ src/main/java/com/bryansiegel/ccsdjobsjava/CcsdJobsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.ccsdjobsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcsdJobsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcsdJobsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryansiegel/ccsdjobsjava/CcsdJobsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.ccsdjobsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcsdJobsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
