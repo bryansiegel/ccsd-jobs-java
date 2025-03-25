51c5e3d6c5f910e482fd314b7775191f81612ed2
Bryan Siegel <bryansiegel@gmail.com>
2025-03-25 10:19:15 -0700

    design

---
+++ src/main/java/com/bryansiegel/ccsdjobsjava/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.bryansiegel.ccsdjobsjava.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/login")
+    public String login() {
+        return "admin/login";
+    }
+
+    @GetMapping("/login-error/")
+    public String loginError() {
+        return "admin/login-error";
+    }
+
+    @GetMapping("admin/dashboard/")
+    public String dashboard(Authentication authentication, Model model) {
+        model.addAttribute("user", authentication.getName());
+
+        return "admin/dashboard";
+    }
+}
