31b18820b73e1426e556e6fd8c3de1d64d9ea6d1
Bryan Siegel <bryansiegel@gmail.com>
2025-03-25 11:15:21 -0700

    administrative personnel model fields

--- src/main/java/com/bryansiegel/ccsdjobsjava/models/AdministrativePersonnel.java
+++ src/main/java/com/bryansiegel/ccsdjobsjava/models/AdministrativePersonnel.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class AdministrativePersonnel {
@@ -11,25 +15,141 @@ public class AdministrativePersonnel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //job code
-    //reference code
-    //division_unit
-    //classification
-    //terms of employment
-    //FLSA status
+    private String jobCode;
+    private String referenceCode;
+    private String divisionUnit;
+    private String classification;
+    private String termsOfEmployment;
+    private String flsaStatus;
+    private String positionSummary;
+    private String essentialDutiesAndResponsibilities;
+    private String positionExpectations;
+    private String positionRequirements;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
-    //position summary
-    //essential duties and responsibilities
-    //position expectations
+    public Long getId() {
+        return id;
+    }
 
-    //position requirements// don't create
-    //education and training
-    //licenses and certifications
-    //experience
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    //Job Revision Information// don't create
-    //revised
-    //created
+    public String getJobCode() {
+        return jobCode;
+    }
 
+    public void setJobCode(String jobCode) {
+        this.jobCode = jobCode;
+    }
 
+    public String getReferenceCode() {
+        return referenceCode;
+    }
+
+    public void setReferenceCode(String referenceCode) {
+        this.referenceCode = referenceCode;
+    }
+
+    public String getDivisionUnit() {
+        return divisionUnit;
+    }
+
+    public void setDivisionUnit(String divisionUnit) {
+        this.divisionUnit = divisionUnit;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getTermsOfEmployment() {
+        return termsOfEmployment;
+    }
+
+    public void setTermsOfEmployment(String termsOfEmployment) {
+        this.termsOfEmployment = termsOfEmployment;
+    }
+
+    public String getFlsaStatus() {
+        return flsaStatus;
+    }
+
+    public void setFlsaStatus(String flsaStatus) {
+        this.flsaStatus = flsaStatus;
+    }
+
+    public String getPositionSummary() {
+        return positionSummary;
+    }
+
+    public void setPositionSummary(String positionSummary) {
+        this.positionSummary = positionSummary;
+    }
+
+    public String getEssentialDutiesAndResponsibilities() {
+        return essentialDutiesAndResponsibilities;
+    }
+
+    public void setEssentialDutiesAndResponsibilities(String essentialDutiesAndResponsibilities) {
+        this.essentialDutiesAndResponsibilities = essentialDutiesAndResponsibilities;
+    }
+
+    public String getPositionExpectations() {
+        return positionExpectations;
+    }
+
+    public void setPositionExpectations(String positionExpectations) {
+        this.positionExpectations = positionExpectations;
+    }
+
+    public String getPositionRequirements() {
+        return positionRequirements;
+    }
+
+    public void setPositionRequirements(String positionRequirements) {
+        this.positionRequirements = positionRequirements;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "AdministrativePersonnel{" +
+                "id=" + id +
+                ", jobCode='" + jobCode + '\'' +
+                ", referenceCode='" + referenceCode + '\'' +
+                ", divisionUnit='" + divisionUnit + '\'' +
+                ", classification='" + classification + '\'' +
+                ", termsOfEmployment='" + termsOfEmployment + '\'' +
+                ", flsaStatus='" + flsaStatus + '\'' +
+                ", positionSummary='" + positionSummary + '\'' +
+                ", essentialDutiesAndResponsibilities='" + essentialDutiesAndResponsibilities + '\'' +
+                ", positionExpectations='" + positionExpectations + '\'' +
+                ", positionRequirements='" + positionRequirements + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
